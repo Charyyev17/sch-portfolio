c6d43e949efce4eb31a65e832898aee66234b0f3
Charyyev17 <charyyevsylap@gmail.com>
2023-06-18 17:06:17 +0300

    update on projects

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -6,34 +6,46 @@ type Props = {};
 function Projects({}: Props) {
   const projects = [
     {
+      img: "https://user-images.githubusercontent.com/66562485/246669781-d9a8a463-a5fd-4b5b-b76f-07719ba83639.png",
+      name: "Booking App",
+      url: "soon...",
+      summary:
+        "This Website developed by using Reactjs, Nodejs, MongoDB",
+    },
+    {
       img: "https://user-images.githubusercontent.com/66562485/241756642-925f5d0b-db5e-4ec2-a15b-8855d7859933.png",
       name: "Travel Website",
       url: "https://basic-travel-website.netlify.app/",
-      summary: "This Website developed by using Reactjs, TailwindCss & Fully Responsive",
+      summary:
+        "This Website developed by using Reactjs, TailwindCss & Fully Responsive",
     },
     {
       img: "https://user-images.githubusercontent.com/66562485/241718018-a24235dd-fefe-4763-84c4-ba8d86048635.png",
       name: "Gyzylburc - Youtube Clone",
       url: "https://gyzylburc.netlify.app/",
-      summary: "Gyzylburc is a youtube clone that developed by using Reactjs, mui, RapidAPI.",
+      summary:
+        "Gyzylburc is a youtube clone that developed by using Reactjs, mui, RapidAPI.",
     },
     {
       img: "https://user-images.githubusercontent.com/66562485/241722411-6d726940-0a37-4ab9-a0a6-d3576de7468e.png",
       name: "Gatnashaly - Video Chat App",
       url: "https://gatnashaly.netlify.app/",
-      summary: "Gatnashaly is a video chat app that developed by using Javascript, HTML/CSS, AgoraRTC.",
+      summary:
+        "Gatnashaly is a video chat app that developed by using Javascript, HTML/CSS, AgoraRTC.",
     },
     {
       img: "https://user-images.githubusercontent.com/66562485/241752674-a6124689-ea09-49e1-97d7-58ec265818b9.png",
       name: "Spotify - Clone",
       url: "https://spotify-clone-by-sch.netlify.app/",
-      summary: "This Spotify clone developed by using Reactjs, TailwindCss JUST for Web versions, NOT recursive and doesn't work healthly on mobile versions.",
+      summary:
+        "This Spotify clone developed by using Reactjs, TailwindCss JUST for Web versions, NOT recursive and doesn't work healthly on mobile versions.",
     },
     {
       img: "https://user-images.githubusercontent.com/66562485/241748528-a812d171-5fb9-402e-825d-1fe090826c5f.png",
       name: "Instagram - Clone",
       url: "https://github.com/Charyyev17/instagram-clone",
-      summary: "This Instagram clone developed by using Reactjs, Redux, TailwindCss, Firebase JUST for Web versions, NOT recursive and doesn't work healthly on mobile versions.",
+      summary:
+        "This Instagram clone developed by using Reactjs, Redux, TailwindCss, Firebase JUST for Web versions, NOT recursive and doesn't work healthly on mobile versions.",
     },
     {
       img: "https://user-images.githubusercontent.com/66562485/242041233-f2a2284d-1b47-4ddf-9486-6bdbe0ee5dac.png",
